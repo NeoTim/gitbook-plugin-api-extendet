108b5b9796917e9beee4b4b78e8659a0947b6a3c
Emanuel Bennici <benniciemanuel78@gmail.com>
2018-05-24 00:00:37 +0200

    Add 'codetabs' Tag to 'index.js'

--- index.js
+++ index.js
@@ -60,6 +60,31 @@ module.exports = {
                     return container.outerHTML
                 })
             }
+        },
+        codetabs: {
+            blocks: ['language'],
+            process: function(parentBlock) {
+                var blocks = [parentBlock].concat(parentBlock.blocks);
+                var tabsContent = '';
+                var tabsHeader = '';
+
+                blocks.forEach(function(block, i) {
+                    var isActive = (i == 0);
+
+                    if (!block.kwargs.name) {
+                        throw new Error('Code tab requires a "name" property');
+                    }
+
+                    tabsHeader += createTab(block, i, isActive);
+                    tabsContent += createTabBody(block, i, isActive);
+                });
+
+
+                return '<div class="codetabs">' +
+                    '<div class="codetabs-header">' + tabsHeader + '</div>' +
+                    '<div class="codetabs-body">' + tabsContent + '</div>' +
+                    '</div>';
+            }
         }
     }
 };
